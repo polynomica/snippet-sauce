25f205acde552072749829d51cdeef741ecdc5e2
SUYASH VASHISHTHA <suyashvashishtha@gmail.com>
2021-12-23 23:35:32 +0530

    added footer and logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,39 @@
 import navStyles from '../styles/Navbar.module.css'
+import ssLogo from '../components/sslogo.png'
 
 export default function Navbar() {
     return (
         <nav className={navStyles.navbar}>
-            <a className={navStyles.navLinks} href='/'>Snippet Sauce</a>
+            <a className={`${navStyles.navLinks} ${navStyles.hero}`} href='/'>
+                <img src={ssLogo.src} />
+                Snippet Sauce
+            </a>
 
             <div className={navStyles.navContent}>
-                <a href="#Languages" className={`${navStyles.navLinks} ${navStyles.navLinksActive}`}>Languages</a>
-                <a href="#Contribute" className={navStyles.navLinks}>Contribute</a>
+                <span className={`${navStyles.navLinks}  ${navStyles.langPicker}`}>Languages
+                    <div className={navStyles.languageHolder}>
+                        <a className={navStyles.langHolderLinks} href="#">Python</a>
+                        <a className={navStyles.langHolderLinks} href="#">Java</a>
+                        <a className={navStyles.langHolderLinks} href="#">JavaScript</a>
+                        <a className={navStyles.langHolderLinks} href="#">Python</a>
+                        <a className={navStyles.langHolderLinks} href="#">Java</a>
+                        <a className={navStyles.langHolderLinks} href="#">JavaScript</a>
+                        <a className={navStyles.langHolderLinks} href="#">Python</a>
+                        <a className={navStyles.langHolderLinks} href="#">Java</a>
+                        <a className={navStyles.langHolderLinks} href="#">JavaScript</a>
+                        <a className={navStyles.langHolderLinks} href="#">Python</a>
+                        <a className={navStyles.langHolderLinks} href="#">Java</a>
+                        <a className={navStyles.langHolderLinks} href="#">JavaScript</a>
+                        <a className={navStyles.langHolderLinks} href="#">Python</a>
+                        <a className={navStyles.langHolderLinks} href="#">Java</a>
+                        <a className={navStyles.langHolderLinks} href="#">JavaScript</a>
+                        <a className={navStyles.langHolderLinks} href="#">Python</a>
+                        <a className={navStyles.langHolderLinks} href="#">Java</a>
+                        <a className={navStyles.langHolderLinks} href="#">JavaScript</a>
+                    </div>
+                </span>
+
+                <a href="#Contribute" className={`${navStyles.navLinks} ${navStyles.navLinksActive}`}>Contribute</a>
                 <a href="#About" className={navStyles.navLinks}>About</a>
                 <a href="#Profile" className={navStyles.navLinks}>Profile</a>
             </div>
--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,12 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 
-export default function Home() {
+export default function HomeScreen() {
   return (
-    <div >
-      Hi
+    <div className={styles.homeScreen}>
+      <div className={styles.contentArea}>
+        Hi
+      </div>
     </div>
   )
 }
