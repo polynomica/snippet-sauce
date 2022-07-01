c575d3f40fc3c72a91ddaf2167bbcea83e7f2d04
SUYASH VASHISHTHA <suyashvashishtha@gmail.com>
2022-07-02 01:27:34 +0530

    ga fix

--- pages/_app.js
+++ pages/_app.js
@@ -20,11 +20,12 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Script
+        id="my-ga-script-link"
         strategy='lazyOnload'
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
       />
 
-      <Script strategy='lazyOnload'>
+      <Script id="my-ga-script-code" strategy='lazyOnload'>
         {`
           window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments)}
